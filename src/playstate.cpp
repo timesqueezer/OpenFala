e7ce50bdc809b750c3f123010ee91b974e37914e
Matz Radloff <matzradloff@gmx.de>
2010-04-13 18:15:30 +0200

    added a selector to the server

--- src/playstate.cpp
+++ src/playstate.cpp
@@ -122,7 +122,7 @@ void PlayState::HandleEvents(){
         if (Event.Type == sf::Event::MouseButtonPressed) {
             if (mode==1) {
                 if (MouseInPlayableArea()) {
-                    SendPacket << m_cl_id << (sf::Uint16) (GetMouseBlock('x','p') - (mGameEngine->m_width / m_ratio - 20)/2) << GetMouseBlock('y','p') << "block" << m_name;
+                    SendPacket << 1 << (sf::Uint16) (GetMouseBlock('x','p') - (mGameEngine->m_width / m_ratio - 20)/2) << GetMouseBlock('y','p');
                     Socket.Send(SendPacket, m_bindaddress, m_port);
                     SendPacket.Clear();
                 }
@@ -133,9 +133,9 @@ void PlayState::HandleEvents(){
 		    float mouse_y = (m_mpos[0]->m_Shape.GetPosition().y)/m_ratio;
 
             if ((GetMouseBlock('x', 'p') != mouse_x) or (GetMouseBlock('y', 'p') != mouse_y)) {
-		    	SendPacket << m_cl_id << GetMouseBlock('x', 'p') << GetMouseBlock('y', 'p') << "mouse" << m_name;
-		    	Socket.Send(SendPacket, m_bindaddress, m_port);
-		    	SendPacket.Clear();
+		    	//SendPacket << m_cl_id << GetMouseBlock('x', 'p') << GetMouseBlock('y', 'p') << "mouse" << m_name;
+		    	//Socket.Send(SendPacket, m_bindaddress, m_port);
+		    	//SendPacket.Clear();
 		    	m_mpos[0]->m_Shape.SetPosition((GetMouseBlock('x', 'p')+GetNonPlayableAreaSize())*m_ratio, GetMouseBlock('y', 'p')*m_ratio);
 		    }
 		}
