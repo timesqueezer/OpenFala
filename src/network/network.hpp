869c7c53246ddaea0de51de06be3cb9a24a479f2
Sven-Hendrik Haase <sh@lutzhaase.com>
2010-04-14 04:43:16 +0200

    Using constants now for network packets, other cleanups

---
+++ src/network/network.hpp
@@ -0,0 +1,15 @@
+#ifndef NETWORK_HPP_INCLUDED
+#define NETWORK_HPP_INCLUDED
+
+#include <SFML/Network.hpp>
+
+// packet_t is the header of each sent or received packet on either side,
+// client or server. It determines which action to take on each side.
+// If the packet is sent by the client, the server interprets it as an request.
+// If the packet is sent by the server, the client interprets it as a command.
+const sf::Uint16 PACKET_NONE = 0;
+const sf::Uint16 PACKET_MOUSE = 1;
+const sf::Uint16 PACKET_BUILD = 2;
+const sf::Uint16 PACKET_HANDSHAKE = 3;
+
+#endif
--- src/networking/server.cpp
+++ src/networking/server.cpp
@@ -2,52 +2,30 @@
 #include <string>
 #include <SFML/Network.hpp>
 
+enum PacketType {
+    LOL,
+    OMFG,
+    NIX
+};
+
 int main() {
-
-    enum PacketType {
-        Handshake,
-        Msg,
-        Default
-    };
-
-    sf::SelectorUDP Selector;
-
     sf::SocketUDP Listener;
+    sf::Packet Packet;
+
+    sf::IPAddress Address;
+    sf::Uint16 port;
 
     if (!Listener.Bind(12345)) {
         std::cout << "Fail while binding the listening Socket"; 
     }
 
-    Selector.Add(Listener);
+    Listener.Receive(Packet, Address, port);
 
-    //Listener.SetBlocking(true);
+    sf::Uint16 bar = 41;
+    Packet >> bar;
+    std::cout << "bar: " << bar;
 
-    while (true) {
-    
-        unsigned int NbSockets = Selector.Wait(0.2f);
-
-        if (NbSockets) {
-            
-            sf::SocketUDP cSocket = Selector.GetSocketReady(0); // Stands for currentSocket
-
-            sf::Packet Packet;
-            sf::IPAddress Address;
-            unsigned short port;
-            cSocket.Receive(Packet, Address, port);
-            std::string data;
-            PacketType t_hs = Default;
-            PacketType t_msg = Default;
-            int i = 0;
-            Packet >> i;
-            std::cout << i;
-            //std::cout << "Client [" << Address << ":" << port << "] sent: " << data << " with type: " << t_hs << " " << t_msg << std::endl;
-            
-            Packet.Clear();
-            //sf::Sleep(0.2f);
-
-        }
-    
-    }
     Listener.Close();
+
     return EXIT_SUCCESS;
 }
