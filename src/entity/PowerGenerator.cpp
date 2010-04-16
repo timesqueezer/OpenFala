fed5de728e84e8531d4519ff2d313a4a3bc1e655
Matz Radloff <matzradloff@gmx.de>
2010-04-16 16:15:47 +0200

    mend
    Added a PowerGenerator with animation

---
+++ src/entity/PowerGenerator.cpp
@@ -0,0 +1,29 @@
+#include "PowerGenerator.hpp"
+#include <iostream>
+
+PowerGenerator::PowerGenerator() {
+    mAnimationFPS = 3;
+    mAnimationIndex = 0;
+    mPosX = 0;
+    mPosY = 0;
+}
+
+void PowerGenerator::Update(float dt) {
+    // Update Life Time
+    mLifeTime += dt;
+
+    // Set Image Pointer before Animation, as setting the Image changes the Subrect
+    mSprite.SetImage(* ResourceManager::get_mutable_instance().GetImage(mImageKey));
+
+    // Update Animation
+    UpdateAnimation();
+}
+
+sf::Uint16 PowerGenerator::GetPower() {
+    return mPower;
+}
+
+void PowerGenerator::SetPosition(sf::Vector2f pos) {
+    mPosX = pos.x+0.25f;
+    mPosY = pos.y+0.5f;
+}
